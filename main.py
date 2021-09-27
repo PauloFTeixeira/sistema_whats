f9ba9e35bc5e722357fc8fce11a440697b693dd1
Paulo <trading.teixeira@gmail.com>
2021-09-27 08:44:03 -0300

    atualizacao do arquivo main

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
+from colorama import Fore, Style, init
 from time import sleep
 import random
 
-
+init(autoreset=True)
 contatos = []
 dict_mensagens = {}
 link = ("https://web.whatsapp.com/")
@@ -28,7 +29,7 @@ def Informa_contatos():
     contatos.pop()
 
     qtd = len(contatos)
-    print(f'Você informou {qtd} contatos.')
+    print(Fore.BLUE + f'Você informou {qtd} contatos.')
 
 
 def criar_mensagem():
@@ -42,7 +43,7 @@ def criar_mensagem():
         
     dict_mensagens.pop(contador)
     for msg in dict_mensagens:
-        print(msg, dict_mensagens[msg])
+        print(Fore.BLUE + f'{msg}, {dict_mensagens[msg]}')
 
 
 def enviar_mensagem_buscando_contato():
@@ -50,27 +51,30 @@ def enviar_mensagem_buscando_contato():
     Envia mensagem utilizando uma lista de contatos préviamente informada, através de informação direta ou busca por contatos em arquivo .csv
     
     """
-    for contato in contatos:
-        campo_pesquisa = browser.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]')
-        campo_pesquisa.click()
-        sleep(0.25)
-        campo_pesquisa.send_keys(contato)
-        campo_pesquisa.send_keys(Keys.ENTER)
-        
-        for chave in dict_mensagens:
-            msg = dict_mensagens[chave]
-            tempo_entre_mensagens = float(len(msg)) * 0.15
-            sleep(tempo_entre_mensagens)
-            campo_digitacao = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[1]/div/div[2]')
-            campo_digitacao.click()
-            campo_digitacao.send_keys(msg)
-            sleep(0.05)
-            enviar = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]/button').click()
+    try:
+        for contato in contatos:
+            campo_pesquisa = browser.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]')
+            campo_pesquisa.click()
+            sleep(0.25)
+            campo_pesquisa.send_keys(contato)
+            campo_pesquisa.send_keys(Keys.ENTER)
             
-        print(f'Mensagem enviada para {contato}')
-    sleep(10)
-        
-    browser.quit()
+            for chave in dict_mensagens:
+                msg = dict_mensagens[chave]
+                tempo_entre_mensagens = float(len(msg)) * 0.15
+                sleep(tempo_entre_mensagens)
+                campo_digitacao = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/div/div[2]/div[1]/div/div[2]')
+                campo_digitacao.click()
+                campo_digitacao.send_keys(msg)
+                sleep(0.05)
+                enviar = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/div/div[2]/div[2]/button')
+                enviar.click()
+            print(Fore.GREEN + f'Mensagem enviada para {contato}')
+        sleep(10)
+            
+        browser.quit()
+    except:
+        print('Erro ao enviar mensagem, contate o Administrador do sistema!')
 
 
 #  Execução
@@ -79,28 +83,31 @@ Informa_contatos()
 criar_mensagem()
 navegador = int(input("Qual navegador desaja usar? (1)Chrome, (2)Firefox, (0)Cancelar: "))
 
-if navegador == 1:
-        browser = webdriver.chrome(ChromeDriverManager().install())
-        browser.get(link)
-        print("Aguardando conexão com o WhatsApp...")
-        sleep(1)
-        manter_conectado = browser.find_element_by_xpath('/html/body/div/div[1]/div/div[2]/div[1]/div/div[3]/label/input').click()
-        while len(browser.find_elements_by_id("side")) < 1:
+try:
+    if navegador == 1:
+            browser = webdriver.Chrome(ChromeDriverManager().install())
+            browser.get(link)
+            print(Fore.BLUE + "Aguardando conexão com o WhatsApp...")
             sleep(1)
-        print("Conexão bem sucedida!")
-elif navegador == 2:
-        browser = webdriver.firefox(executable_path=GeckoDriverManager().install())
-        browser.get(link)
-        print("Aguardando conexão com o WhatsApp...")
-        sleep(1)
-        manter_conectado = browser.find_element_by_xpath('/html/body/div/div[1]/div/div[2]/div[1]/div/div[3]/label/input').click()
-        while len(browser.find_elements_by_id("side")) < 1:
+            manter_conectado = browser.find_element_by_xpath('/html/body/div/div[1]/div/div[2]/div[1]/div/div[3]/label/input').click()
+            while len(browser.find_elements_by_id("side")) < 1:
+                sleep(1)
+            print(Fore.GREEN + "Conexão bem sucedida!")
+    elif navegador == 2:
+            browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+            browser.get(link)
+            print(Fore.BLUE + "Aguardando conexão com o WhatsApp...")
             sleep(1)
-        print("Conexão bem sucedida!")
-elif navegador == 0:
-        exit()
-else:
-    abrir_navegador()
+            manter_conectado = browser.find_element_by_xpath('/html/body/div/div[1]/div/div[2]/div[1]/div/div[3]/label/input').click()
+            while len(browser.find_elements_by_id("side")) < 1:
+                sleep(1)
+            print(Fore.GREEN + "Conexão bem sucedida!")
+    elif navegador == 0:
+            exit()
+    else:
+        abrir_navegador()
+except:
+    print(Fore.RED + 'Conexão mal sucedida!')
 
 enviar_mensagem_buscando_contato()
 
